df54c27f97b7928b318c967372a375fdbcc9b209
a-t-0 <eai@protonmail.com>
2021-03-18 17:57:42 +0100

    Created bounty hunter repository that attacks unit test.

---
+++ solve_sqrt.py
@@ -0,0 +1,9 @@
+class Main:
+    
+    def __init__(self):
+        pass
+        
+    def calc_sqrt(self, x):
+        #return x**0.5 # sponsor doesn't have a solution yet.
+        return None
+        
